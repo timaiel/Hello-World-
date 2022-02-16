1edf051d77d2dffa462c7322e8360501194a7a41
timaiel <99826980+timaiel@users.noreply.github.com>
2022-02-16 13:12:57 -0800

    Add files via upload

---
+++ src/FooFirstProgram.java
@@ -0,0 +1,7 @@
+public class FooFirstProgram {
+    public static void main(String[] args){
+       secondClass call = new secondClass();
+       call.hi();
+    }
+}
+
---
+++ src/secondClass.java
@@ -0,0 +1,5 @@
+public class secondClass {
+    public void hi(){
+        System.out.println("Hello World!");
+    }
+}
